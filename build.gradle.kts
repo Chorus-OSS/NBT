adbbc0a17e0502e95da01d2e2c8a2c1359954463
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-09 03:12:00 +1200

    init NBT

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform") version "2.1.10"
+}
+
+group = "org.chorus_oss.nbt"
+version = "1.0-SNAPSHOT"
+description = "NBT"
+
+repositories {
+    mavenCentral()
+}
+
+@OptIn(ExperimentalWasmDsl::class)
+kotlin {
+    jvm()
+    js()
+    wasmJs()
+    wasmWasi()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.kotlinx.io)
+            }
+        }
+
+        val commonTest by getting {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
---
+++ src/commonMain/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.chorus_oss.nbt
+
+fun main() {
+    println("Hello World!")
+}
